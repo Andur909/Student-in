aed03f851cc30fc94c421c5172b644f499dded08
Andur909 <147121202+Andur909@users.noreply.github.com>
2025-06-12 10:08:36 -0400

    Added comment to sign_up.js

--- Sub_Final/static/sign_up.js
+++ Sub_Final/static/sign_up.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", Initial);
 
+//Sets it up
 function Initial() {
     document.getElementById("btn_create").disabled = true;
 
@@ -10,6 +11,7 @@ function Initial() {
     document.getElementById("btn_guest_login").addEventListener("click", Guest);
 }
 
+//Enables Submit (Same as the one found in log in)
 function Enable_submit() {
     var username = document.getElementById("txt_username_signup").value;
     var pass = document.getElementById("txt_password_signup").value;
@@ -17,10 +19,12 @@ function Enable_submit() {
     document.getElementById("btn_create").disabled = (username == "" || pass == "");
 }
 
+//Sends User back to Login
 function LogIn() {
     window.location.href = "/";
 }
 
+//To Enter as Guest
 function Guest() {
     alert("Welcome Guest! You are limited now.");
     window.location.href = "/main";
