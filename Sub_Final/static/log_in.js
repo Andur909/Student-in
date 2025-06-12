1f826d5dffb4b231f1f8bd58e4448c721d8204e3
Andur909 <147121202+Andur909@users.noreply.github.com>
2025-06-12 10:02:25 -0400

    Added Comments to log_in.js

--- Sub_Final/static/log_in.js
+++ Sub_Final/static/log_in.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", Initial);
 
+//Sets it up
 function Initial() {
     document.getElementById("btn_submit_login").disabled = true;
 
@@ -10,6 +11,7 @@ function Initial() {
     document.getElementById("btn_New_login").addEventListener("click", SignUp);
 }
 
+//Enables submit when the requirements are met
 function Enable_submit() {
     var username = document.getElementById("txt_username_login").value;
     var pass = document.getElementById("txt_password_login").value;
@@ -17,10 +19,12 @@ function Enable_submit() {
     document.getElementById("btn_submit_login").disabled = (username == "" || pass == "");
 }
 
+//Sign Up CLicked
 function SignUp() {
     window.location.href = "/sign_up";
 }
 
+//Guest Clicked
 function Guest() {
     alert("Welcome Guest! You are limited now.");
     window.location.href = "/main";
