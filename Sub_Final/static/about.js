43aadf69d36ce747476fa1a9ccf9144035d674f4
Andur909 <147121202+Andur909@users.noreply.github.com>
2025-06-12 09:48:36 -0400

    Added Comments to About.js

--- Sub_Final/static/about.js
+++ Sub_Final/static/about.js
@@ -4,7 +4,7 @@ function Initial() {
     document.getElementById("btn_back").addEventListener("click", back);
 }
 
-
+//Sends back the user
 function back() {
     window.location.href = "/main";
 }
